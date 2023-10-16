fd70e263869adc965d0ba8ec1a9c0881f7437d3f
Shreya Verma <shreyaverma@Shreyas-MacBook-Pro.local>
2023-10-16 13:44:48 +0530

    testing deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from mlProject.pipeline.prediction import PredictionPipeline
 
-app = Flask(__name__) # initializing a flask
+app = Flask(__name__) # initializing a FLASK app
 
 @app.route('/',methods=['GET'])  # route to display the home page
 def homePage():
